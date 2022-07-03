6fe7eb72815e046dea736721ebf2f26e90b5ebfc
jamie <jamietam2000@gmail.com>
2022-07-03 22:07:09 +0800

    fetch data 1

--- lib/album_list_viewModel.dart
+++ lib/album_list_viewModel.dart
@@ -12,7 +12,26 @@ abstract class AlbumListViewModel extends State<AlbumList> {
   bool isLoading = false;
 
   Future<void> getAlbum() async {
-    
+    changeLoading();
+    final response = await http.get(Uri.parse(Url));
+    if (response.statusCode == 200) {
+      final jsonBody = jsonDecode(response.body);
+      if (jsonBody is List) {
+        model = jsonBody.map((e) => Model.fromJson(e)).toList().cast<Model>();
+      }
+    } else {
+        throw Exception('Failed to load album');
+    }
+
+
   }
+
+void changeLoading() {
+  setState(() {
+    isLoading = !isLoading;
+  }); 
+}
+
+
 
 }
