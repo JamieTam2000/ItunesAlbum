826f7089e112b5c6ed35a56d53852f4a1af6b384
jamie <jamietam2000@gmail.com>
2022-07-03 23:11:50 +0800

    fetch data 2

--- lib/album_list_viewModel.dart
+++ lib/album_list_viewModel.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:html';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -8,23 +7,25 @@ import 'package:album/album_list.dart';
 
 abstract class AlbumListViewModel extends State<AlbumList> {
   final Url = "https://itunes.apple.com/search?term=jack+johnson&entity=album";
-  List<Model> model = [];
+  
   bool isLoading = false;
-
+  
   Future<void> getAlbum() async {
     changeLoading();
     final response = await http.get(Uri.parse(Url));
     if (response.statusCode == 200) {
-      final jsonBody = jsonDecode(response.body);
-      if (jsonBody is List) {
-        model = jsonBody.map((e) => Model.fromJson(e)).toList().cast<Model>();
+      final models = json.decode(response.body.toString());
+      var model = models
+              .map((e) => Model.fromJson(e))
+              .toList()
+              .cast<Model>();
       }
-    } else {
+     else {
         throw Exception('Failed to load album');
-    }
-
-
-  }
+     }
+    changeLoading();
+    ;
+}
 
 void changeLoading() {
   setState(() {
